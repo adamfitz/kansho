8d90671702f794c4db2d0b17f62155e3d528e9f0
Adam Fitzgerald <adam@networknoob.net>
2025-11-01 17:28:00 +1100

    add delete button

--- ui/viewsMangaList.go
+++ ui/viewsMangaList.go
@@ -5,8 +5,7 @@ import (
 
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/container"
-
-	//"fyne.io/fyne/v2/layout"
+	"fyne.io/fyne/v2/dialog"
 	"fyne.io/fyne/v2/widget"
 )
 
@@ -24,7 +23,11 @@ type MangaListView struct {
 	Card fyne.CanvasObject
 
 	// List is the scrollable list widget showing manga titles
-	List *widget.List
+	List         *widget.List
+	deleteButton *widget.Button // Button to delete the selected manga
+
+	// Track the currently selected manga index
+	selectedIndex int
 
 	// state is a reference to the shared application state
 	// This allows the view to access manga data and notify of selection changes
@@ -46,8 +49,16 @@ type MangaListView struct {
 // - Refresh the list when manga are deleted
 func NewMangaListView(state *KanshoAppState) *MangaListView {
 	view := &MangaListView{
-		state: state,
+		state:         state,
+		selectedIndex: -1, // No selection initially
 	}
+
+	// Create the Delete Manga button
+	view.deleteButton = widget.NewButton("Delete Manga", func() {
+		view.onDeleteButtonClicked()
+	})
+	// Initially disable the delete button since nothing is selected
+	view.deleteButton.Disable()
 
 	// Sort manga alphabetically by title for consistent display
 	// This makes it easier for users to find specific manga
@@ -84,19 +95,29 @@ func NewMangaListView(state *KanshoAppState) *MangaListView {
 	// Set up the selection handler
 	// This is called when a user clicks on a list item
 	view.List.OnSelected = func(id widget.ListItemID) {
+		// Store the selected index
+		view.selectedIndex = int(id)
+
+		// Enable the delete button since something is now selected
+		view.deleteButton.Enable()
+
 		// Notify the app state that selection changed
 		// This will trigger callbacks in other views (like the chapter list)
 		view.state.SelectManga(int(id))
 	}
 
-	// Build the card content with header and list
+	// Build the card content with header, list, and delete button
 	cardContent := container.NewBorder(
 		// Top: Card title and separator
 		container.NewVBox(
 			NewBoldLabel("Manga List"),
 			NewSeparator(),
 		),
-		nil, // Bottom
+		// Bottom: Delete button centered
+		container.NewVBox(
+			NewSeparator(),
+			container.NewCenter(view.deleteButton),
+		),
 		nil, // Left
 		nil, // Right
 		// Center: The scrollable list fills the remaining space
@@ -128,7 +149,52 @@ func (v *MangaListView) refresh() {
 		return v.state.MangaData.Manga[i].Title < v.state.MangaData.Manga[j].Title
 	})
 
+	// Reset selection since indices may have changed
+	v.selectedIndex = -1
+	v.List.UnselectAll()
+	v.deleteButton.Disable()
+
 	// Tell the list widget to refresh its display
 	// This causes UpdateItem to be called again for all visible items
 	v.List.Refresh()
+}
+
+// onDeleteButtonClicked is called when the user clicks the Delete Manga button.
+// This shows a confirmation dialog and deletes the selected manga.
+func (v *MangaListView) onDeleteButtonClicked() {
+	// Validate that something is selected
+	if v.selectedIndex < 0 || v.selectedIndex >= len(v.state.MangaData.Manga) {
+		dialog.ShowInformation(
+			"Delete Manga",
+			"Please select a manga to delete.",
+			v.state.Window,
+		)
+		return
+	}
+
+	// Get the manga title for the confirmation dialog
+	mangaTitle := v.state.MangaData.Manga[v.selectedIndex].Title
+
+	// Show confirmation dialog
+	dialog.ShowConfirm(
+		"Delete Manga",
+		"Are you sure you want to delete \""+mangaTitle+"\"?",
+		func(confirmed bool) {
+			if confirmed {
+				// User confirmed deletion
+				// TODO: Implement actual deletion logic here
+				// For now, just show a placeholder message
+				dialog.ShowInformation(
+					"Delete Manga",
+					"Manga \""+mangaTitle+"\" will be deleted.\n\n(Deletion logic not yet implemented)",
+					v.state.Window,
+				)
+
+				// TODO: Call something like:
+				// v.state.DeleteManga(v.selectedIndex)
+				// This will trigger the RegisterMangaDeletedCallback which calls refresh()
+			}
+		},
+		v.state.Window,
+	)
 }
